b67a9302f8967c3beb0b1ac7c79cb7028799aefd
Sooyoung Yoon <ayoooo240@gmail.com>
2021-12-27 06:08:03 +0900

    💄 Add styles to characters

--- src/pages/characters/index.tsx
+++ src/pages/characters/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import axios from 'axios';
 import useSWR from 'swr';
 import { Character } from '../../types/Characters';
+import styled from '@emotion/styled';
 
 const fetcher = (url: string) => axios(url).then((res) => res.data);
 
@@ -14,22 +15,36 @@ const CharactersIndexPage: NextPage = () => {
     if (!data) return <div>Loading...💫</div>;
 
     return (
-        <div>
+        <CharacterContainer>
             {data.map((character: Character) => {
                 const { age, gender, homePlanet, id, images, name, occupation, sayings, species } = character;
 
                 return (
-                    <div key={`futurama-character-${id}`}>
-                        <img src={images.main} alt='' />
-                        <h2>{name.first} {name.middle} {name.last} ({age})</h2>
-                        <strong><p>{species} from {homePlanet}</p></strong>
+                    <CharacterCard key={`futurama-character-${id}`}>
+                        <Profile src={images.main} alt='' />
+                        <h2>{name.first} {name.middle} {name.last} ({gender[0]}/{age})</h2>
                         <em><q>{sayings[0]}</q></em>
-                        <p>occupation: {occupation}</p>
-                    </div>
+                        <ul><li>{occupation}</li><li>{species} from {homePlanet}</li></ul>
+                    </CharacterCard>
                 )
             })}
-        </div>
+        </CharacterContainer>
     )
 }
+
+const CharacterContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+`
+
+const CharacterCard = styled.div`
+    max-width: 300px;
+`
+
+const Profile = styled.img`
+    width: 100%;
+    aspect-ratio: 9 / 16;
+    object-fit: contain;
+`
 
 export default CharactersIndexPage
